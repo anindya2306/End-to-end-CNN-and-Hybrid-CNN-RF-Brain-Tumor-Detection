4b826a6cf61d21157d431bb580c9cbe58570dc51
Anindya <joyanindya99@gmail.com>
2023-08-08 00:44:06 +0530

    Training block added

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -6,22 +6,6 @@ import tensorflow as tf
 from keras import backend as K
 from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
 
-def recall_m(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-        recall = true_positives / (possible_positives + K.epsilon())
-        return recall
-
-def precision_m(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
-def f1_m(y_true, y_pred):
-    precision = precision_m(y_true, y_pred)
-    recall = recall_m(y_true, y_pred)
-    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 class PrepareBaseModel:
     def __init__(self, config: PrepareBaseModelConfig):
@@ -49,8 +33,8 @@ class PrepareBaseModel:
         feature_in = tf.keras.layers.Dropout(0.3)(model.output)
         feature_in = tf.keras.layers.BatchNormalization()(feature_in)
         prediction = tf.keras.layers.Dense(
-            units=1,
-            activation="sigmoid"
+            units=classes,
+            activation="softmax"
         )(feature_in)
 
         full_model = tf.keras.models.Model(
@@ -60,8 +44,8 @@ class PrepareBaseModel:
 
         full_model.compile(
             optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
-            loss=tf.keras.losses.BinaryCrossentropy(),
-            metrics=["binary_accuracy", f1_m]
+            loss=tf.keras.losses.CategoricalCrossentropy(),
+            metrics=["binary_accuracy", tf.keras.metrics.AUC()]
         )
 
         full_model.summary()
